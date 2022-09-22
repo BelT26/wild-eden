a281aecc9e31f3bf5fecd39cb328033a1affd60c
BelT26 <helen.taylor@hotmail.it>
2022-09-22 07:19:17 +0000

    format garden makeovers and form

--- home/forms.py
+++ home/forms.py
@@ -8,6 +8,5 @@ class ContactForm(forms.Form):
     A form for customers to submit enquiries
     """
     full_name = forms.CharField(max_length=100)
-    email_address = forms.EmailField(max_length=150)
-    subject = forms.CharField(max_length=150)
+    email_address = forms.EmailField(max_length=150)    
     message = forms.CharField(widget=forms.Textarea, max_length=2000)
