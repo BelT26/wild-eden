724a1771fe9743f9b5b134e8ca10e6134900280c
BelT26 <helen.taylor@hotmail.it>
2022-11-28 10:16:14 +0000

    add render url to settings.py

--- Eden/settings.py
+++ Eden/settings.py
@@ -37,9 +37,9 @@ DEBUG = False
 ALLOWED_HOSTS = ["wild-eden.herokuapp.com", "localhost"]
 
 # Add Render.com URL to allowed hosts
- RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
- if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
